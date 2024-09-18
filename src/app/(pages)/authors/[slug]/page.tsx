2c5bb7ea52d1280743b07ace6fc8ff3934469128
Rui S <contact@ruisousa.me>
2024-09-18 15:37:30 +0100

    update: create fields and blocks

--- src/app/(pages)/authors/[slug]/page.tsx
+++ src/app/(pages)/authors/[slug]/page.tsx
@@ -1,43 +1,87 @@
-import React from 'react'
-import { notFound } from 'next/navigation'
+import React from "react";
+import { notFound } from "next/navigation";
 
-import { fetchContentFromAuthor } from "../../../_api/fetchContentFromAuthor"
-import { fetchDoc } from "../../../_api/fetchDoc"
+import { fetchContentFromAuthor } from "../../../_api/fetchContentFromAuthor";
+import { fetchDoc } from "../../../_api/fetchDoc";
+import BackButton from "@/app/_components/BackButton";
+import { getImage } from "@/app/_utilities/getImage";
 
 export default async function ContributorPage({ params: { slug } }) {
-  let contributor = null
-  let postsFromContributor = null
+  let contributor = null;
+  let postsFromContributor = null;
 
   try {
     contributor = await fetchDoc({
-      collection: 'authors',
+      collection: "authors",
       slug,
-    })
+    });
   } catch (err) {
-    console.error(err)
   }
 
   if (!contributor) {
-    notFound()
+    notFound();
   }
 
-  const contributorID = contributor.id
+  const {
+    id,
+    name,
+    role,
+    featuredImage,
+    linkedin,
+    github,
+    medium,
+    x,
+    bio,
+  } = contributor;
 
-  try {
-    postsFromContributor = await fetchContentFromAuthor({ authorID: contributorID })
-  } catch (err) {
-    console.error(err)
-  }
+  postsFromContributor = await fetchContentFromAuthor({ authorID: id })
+  const totalArticles = postsFromContributor.length
 
   return (
     <div>
-      <div>
-        Hello "{contributor.name}"!
+      <div style={{ background: "purple" }}>
+        <BackButton />
         <div>
-          <h1>Posts by "{contributor.name}</h1>
+          {/*Left column*/}
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {/* Image container */}
+            <div style={{ width: 120, height: 120, marginRight: '20px' }}>
+              <img src={getImage(featuredImage)} alt={name} style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }} />
+            </div>
+
+            {/* Info container */}
+            <div>
+              <h5 style={{ margin: 0 }}>{name}</h5>
+              <h6 style={{ margin: 0, fontWeight: 'normal' }}>{role}</h6>
+
+              {/* Social links */}
+              <div style={{ marginTop: '10px' }}>
+                <a href={linkedin} target="_blank" rel="noopener noreferrer">LinkedIn</a> |
+                <a href={github} target="_blank" rel="noopener noreferrer"> GitHub</a> |
+                <a href={medium} target="_blank" rel="noopener noreferrer"> Medium</a> |
+                <a href={x} target="_blank" rel="noopener noreferrer"> X</a>
+              </div>
+            </div>
+          </div>
+          {/*  Right column */}
+          <div>
+            {bio}
+          </div>
+        </div>
+
+
+      </div>
+
+      {/* Content block*/}
+      <div style={{ background: "white", color: "black" }}>
+        <div>{totalArticles} Articles</div>
+        <div>
           <pre>{JSON.stringify(postsFromContributor, null, 2)}</pre>
+          <div>
+
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
