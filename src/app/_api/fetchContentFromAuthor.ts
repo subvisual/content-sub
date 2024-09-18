e29ac858db13599fc3743c55a46fed1593ad1337
Rui S <contact@ruisousa.me>
2024-09-18 15:35:26 +0100

    update: error handling and output type
    - function now returns an array of existing content;
    - fix type hinting;
    - added error handling to declutter page components;

--- src/app/_api/fetchContentFromAuthor.ts
+++ src/app/_api/fetchContentFromAuthor.ts
@@ -1,30 +1,34 @@
 import { CONTENT_FROM_AUTHOR } from '../_graphql/contentFromAuthor'
 import { GRAPHQL_API_URL } from './shared'
 
-export async function fetchContentFromAuthor<T>({
-  authorID,
-}: {
-  authorID: string
-}): Promise<{ Blogposts: T; PodcastEpisodes: T; CaseStudies: T; TalksAndRoundtables: T }> {
-  return await fetch(`${GRAPHQL_API_URL}/api/graphql`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: CONTENT_FROM_AUTHOR,
-      variables: { authorID: authorID },
-    }),
-  })
-    ?.then(res => res.json())
-    ?.then(res => {
-      if (res.errors) throw new Error(res?.errors?.[0]?.message ?? 'Error fetching content.')
-
-      const Blogposts = res?.data?.Blogposts?.docs || []
-      const PodcastEpisodes = res?.data?.PodcastEpisodes?.docs || []
-      const CaseStudies = res?.data?.CaseStudies?.docs || []
-      const TalksAndRoundtables = res?.data?.TalksAndRoundtables?.docs || []
-
-      return { Blogposts, PodcastEpisodes, CaseStudies, TalksAndRoundtables }
+export async function fetchContentFromAuthor<T>({ authorID }: { authorID: string }): Promise<T[]> {
+  try {
+    return await fetch(`${GRAPHQL_API_URL}/api/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: CONTENT_FROM_AUTHOR,
+        variables: { authorID: authorID },
+      }),
     })
+      ?.then(res => res.json())
+      ?.then(res => {
+        if (res.errors) throw new Error(res?.errors?.[0]?.message ?? 'Error fetching content.')
+
+        const Blogposts = res?.data?.Blogposts?.docs || []
+        const PodcastEpisodes = res?.data?.PodcastEpisodes?.docs || []
+        const CaseStudies = res?.data?.CaseStudies?.docs || []
+        const TalksAndRoundtables = res?.data?.TalksAndRoundtables?.docs || []
+
+        const content = { Blogposts, PodcastEpisodes, CaseStudies, TalksAndRoundtables }
+
+        return Object.values(content).filter(
+          innerArray => Array.isArray(innerArray) && innerArray.length > 0,
+        )
+      })
+  } catch (err: unknown) {
+    return []
+  }
 }
