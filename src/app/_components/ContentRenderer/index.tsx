ed0c3980778482c0a69ed2687fc37da79bdb080d
Rui S <contact@ruisousa.me>
2024-09-20 10:05:32 +0100

    update: add DOMpurify to ContentRenderer
    - added DOMpurify to sanitize HTML to prevent potential XSS
      vulnerabilities

--- src/app/_components/ContentRenderer/index.tsx
+++ src/app/_components/ContentRenderer/index.tsx
@@ -1,5 +1,8 @@
-export default function ContentRenderer({ content_html }: { content_html: string }){
-  return (
-    <div dangerouslySetInnerHTML={{ __html: content_html }}/>
-  )
+import DOMpurify from 'dompurify'
+
+export default function ContentRenderer({ content_html }: { content_html: string }) {
+  // Sanitize HTML content to prevent XSS vulnerabilities.
+  const sanitizedContent = DOMpurify.sanitize(content_html)
+
+  return <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
 }
