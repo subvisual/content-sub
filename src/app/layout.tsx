d390ce68752761eb3edec283d9ad9040cb08aa50
Rui S <contact@ruisousa.me>
2024-09-23 17:57:00 +0100

    setup css global variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Footer } from './_components/Footer'
 import { Header } from './_components/Header'
 import { mergeOpenGraph } from './_utilities/mergeOpenGraph'
 
+import './_css/globals.css'
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
