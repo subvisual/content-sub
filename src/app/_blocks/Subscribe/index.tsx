3a4e5f26e510d19155e938d3c0020e7fe9a77fb9
Rui S <contact@ruisousa.me>
2024-09-27 12:16:27 +0100

    format and fix
    - updated font variables to reflect changes to globals.css

--- src/app/_blocks/Subscribe/index.tsx
+++ src/app/_blocks/Subscribe/index.tsx
@@ -7,7 +7,7 @@ export function Subscribe() {
         <img className={styles.image} src={'/subscribe-card-image.png'} />
       </div>
       <div className={styles.CTABlock}>
-        <h5>Subscribe to Subvisual Inspo</h5>
+        <h5>Subscribe to <br/> Subvisual Inspo</h5>
         <div className={styles.emailContainer}>
           <input className={styles.emailInput} placeholder={"Your email"}></input>
           {/* TODO: Implement the hook to subscribe */}
