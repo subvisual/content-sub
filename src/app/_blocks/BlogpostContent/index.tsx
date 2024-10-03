dbacad767480d4597fe295f3a883e9be202aacf8
Rui S <contact@ruisousa.me>
2024-10-03 15:56:59 +0100

    linting

--- src/app/_blocks/BlogpostContent/index.tsx
+++ src/app/_blocks/BlogpostContent/index.tsx
@@ -1,14 +1,9 @@
 'use client'
 
-import DOMPurify from 'isomorphic-dompurify'
-
 import { Blogpost } from '../../../payload/payload-types'
-import EpisodeFeaturedImage from '../../_components/EpisodeFeaturedImage'
+import FeaturedImage from '../../_components/FeaturedImage'
+import { sanitizeAndAddChapters } from '../../_utilities/sanitizeAndAddChapters'
 import styles from './styles.module.css'
-
-import { getImage } from '@/app/_utilities/getImage'
-import { sanitizeAndAddChapters } from "@/app/_utilities/sanitizeAndAddChapters";
-import FeaturedImage from "@/app/_components/FeaturedImage";
 
 export default function BlogpostContent({ post }: { post: Blogpost }) {
   const { summary, content_html, featuredImage } = post
