7fad256782851b3263a5fb3d80fe0da10e4c9243
Rui S <contact@ruisousa.me>
2024-10-03 10:45:42 +0100

    update border color conditional update

--- src/app/_blocks/HubContentGrid/page.tsx
+++ src/app/_blocks/HubContentGrid/page.tsx
@@ -1,44 +1,53 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 
-import ContentCard from '../../_components/ContentCard'
-import ContentNavBar from '../../_components/ContentNavBar/ContentNavBar'
-import { ContentTypeArrays } from '../../_interfaces/ContentTypeArrays'
-import { filterArticles } from '../../_utilities/filterArticles'
-import styles from './styles.module.css'
+import ContentCard from "../../_components/ContentCard";
+import ContentNavBar from "../../_components/ContentNavBar/ContentNavBar";
+import { ContentTypeArrays } from "../../_interfaces/ContentTypeArrays";
+import { filterArticles } from "../../_utilities/filterArticles";
+import styles from "./styles.module.css";
 
 const colorMap = {
-  All: 'var(--dark-rock-800)',
-  Blogposts: 'var(--sub-blue-600)',
-  PodcastEpisodes: 'var(--sub-purple-600)',
-  CaseStudies: 'var(--sub-orange-800)',
-  TalksAndRoundtables: 'var(--sub-purple-300)',
-}
+  All: "var(--dark-rock-800)",
+  Blogposts: "var(--sub-blue-600)",
+  PodcastEpisodes: "var(--sub-purple-600)",
+  CaseStudies: "var(--sub-orange-800)",
+  TalksAndRoundtables: "var(--sub-purple-300)",
+};
 
 interface HubContentGridProps {
-  articles: ContentTypeArrays
+  articles: ContentTypeArrays;
 }
 
 export default function HubContentGrid({ articles }: HubContentGridProps) {
   // todo: fix rendering when there is no content
 
-  const [activeButton, setActiveButton] = useState<keyof ContentTypeArrays | 'All'>('All')
+  const [activeButton, setActiveButton] = useState<keyof ContentTypeArrays | "All">("All");
+
+  let windowWidth = window.innerWidth;
+
+  window.addEventListener("resize", () => {
+    windowWidth = window.innerWidth;
+    console.log(windowWidth,'< updated!')
+  })
+
 
   const handleActiveButtonChange = (buttonName: keyof ContentTypeArrays) => {
-    setActiveButton(buttonName)
-  }
+    setActiveButton(buttonName);
+  };
 
   const filteredArticles = filterArticles({
     articles: articles,
     filter: activeButton,
-  })
+  });
 
   return (
     <>
       {/* ToDo: add conditional management of border-top-right-radius based on dropdown toggle */}
       <ContentNavBar activeButton={activeButton} onActiveButtonChange={handleActiveButtonChange} />
-      <div className={styles.contentGridContainer} style={{ borderColor: colorMap[activeButton] }}>
+      <div className={styles.contentGridContainer} style={{ borderColor: windowWidth >= 1024 ? colorMap[activeButton] : 'var(--dark-rock-800)' }}>
+        <h1>{windowWidth}</h1>
         <div className={styles.contentGrid}>
           {filteredArticles.map((article, i) => (
             <div className={styles.contentCard} key={i}>
@@ -52,5 +61,5 @@ export default function HubContentGrid({ articles }: HubContentGridProps) {
         </div>
       </div>
     </>
-  )
+  );
 }
