f4846d7579930b7cdf538127a459daf6a3e332fc
Rui S <contact@ruisousa.me>
2024-10-03 15:40:58 +0100

    update FeaturedImage component styling

--- src/app/_blocks/BlogpostContent/index.tsx
+++ src/app/_blocks/BlogpostContent/index.tsx
@@ -8,15 +8,15 @@ import styles from './styles.module.css'
 
 import { getImage } from '@/app/_utilities/getImage'
 import { sanitizeAndAddChapters } from "@/app/_utilities/sanitizeAndAddChapters";
+import FeaturedImage from "@/app/_components/FeaturedImage";
 
 export default function BlogpostContent({ post }: { post: Blogpost }) {
   const { summary, content_html, featuredImage } = post
   const sanitizedContent = sanitizeAndAddChapters(content_html)
-  const imageSource = getImage(featuredImage)
 
   return (
     <div className={styles.container}>
-      <img className={styles.featuredImage} src={imageSource} />
+      <FeaturedImage className={styles.featuredImage} src={featuredImage} />
       <div className={styles.summary}>{summary}</div>
       <div className={styles.content} dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
     </div>
--- src/app/_components/FeaturedImage/index.tsx
+++ src/app/_components/FeaturedImage/index.tsx
@@ -1,7 +1,8 @@
-import { className } from 'postcss-selector-parser'
-
 import { getImage } from '../../_utilities/getImage'
+import { Media } from "@/payload/payload-types";
 
-export default function FeaturedImage({ src, className }) {
-  return <img className={className} src={getImage(src)} />
+import styles from './styles.module.css'
+
+export default function FeaturedImage({ src, className } : { className?: string , src: string | Media}) {
+  return <img className={className ? className : styles.featuredImage} src={getImage(src)} alt={src.alt} />
 }
