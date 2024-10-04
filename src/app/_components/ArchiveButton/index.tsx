59155bc649436c9f05c9aac57179d860a71a3af2
Rui Sousa <contact@ruisousa.me>
2024-10-04 12:29:53 +0100

    add props and linting

--- src/app/_components/ArchiveButton/index.tsx
+++ src/app/_components/ArchiveButton/index.tsx
@@ -1,32 +1,33 @@
-import { BlogpostIcon, CaseStudiesIcon, PodcastIcon, TalksIcon } from "@/app/_icons/icons";
+import { BlogpostIcon, CaseStudiesIcon, PodcastIcon, TalksIcon } from '@/app/_icons/icons'
 
 const iconMap = {
-  blogposts: <BlogpostIcon width={'20'}/>,
-  "podcast-episodes": <PodcastIcon width={'20'}/>,
-  casestudies: <CaseStudiesIcon width={'20'}/>,
-  "talks-and-roundtables": <TalksIcon width={'20'}/>,
-};
-
-import styles from './styles.module.css'
-function formatTitle(text) {
-  return text.replace(/\band\b/g, "&")
-    .split("-")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
+  blogposts: <BlogpostIcon width={'20'} />,
+  'podcast-episodes': <PodcastIcon width={'20'} />,
+  'case-studies': <CaseStudiesIcon width={'20'} />,
+  'talks-and-roundtables': <TalksIcon width={'20'} />,
 }
 
-export default function ArchiveButton({
-                                        collection,
-                                        color,
-                                      }: {
-  collection: string
+import styles from './styles.module.css'
+
+function formatTitle(text: string) {
+  return text
+    .replace(/\band\b/g, '&')
+    .split('-')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ')
+}
+
+interface ArchiveButtonProps {
+  collection: 'blogposts' | 'podcast-episodes' | 'case-studies' | 'talks-and-roundtables'
   color?: string
-}) {
+}
+
+export default function ArchiveButton({ collection, color }: ArchiveButtonProps) {
   return (
-    <a href={`/${collection}`} style={{ color: color ? color : "var(--dark-rock-800)" }}>
+    <a href={`/${collection}`} style={{ color: color ? color : 'var(--dark-rock-800)' }}>
       <div className={styles.container}>
         {iconMap[collection]} {formatTitle(collection)}
       </div>
     </a>
-  );
+  )
 }
