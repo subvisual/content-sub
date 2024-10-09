8f761973064808a883f6b64ca7c1033d9793b727
Rui Sousa <contact@ruisousa.me>
2024-10-09 14:59:58 +0100

    update project dependencies to satisfy payload 3.0 requirements

--- eject.ts
+++ eject.ts
@@ -26,8 +26,8 @@ const eject = async (): Promise<void> => {
   const serverDefaultFile = path.join(__dirname, './src/server.default.ts')
   fs.copyFileSync(serverDefaultFile, serverFile)
 
-  // remove `'plugin:@next/next/recommended', ` from `./.eslintrc.js`
-  const eslintConfigFile = path.join(__dirname, './.eslintrc.js')
+  // remove `'plugin:@next/next/recommended', ` from `./.eslintrc.cjs`
+  const eslintConfigFile = path.join(__dirname, './.eslintrc.cjs')
   const eslintConfig = fs.readFileSync(eslintConfigFile, 'utf8')
   const updatedEslintConfig = eslintConfig.replace(`'plugin:@next/next/recommended', `, '')
   fs.writeFileSync(eslintConfigFile, updatedEslintConfig, 'utf8')
