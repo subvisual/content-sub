8ba6f50bd89d418430a747bdfb68594ff55ace86
Rui Sousa <contact@ruisousa.me>
2024-10-10 12:31:24 +0100

    update content fetchers
    - transition to payload local API

---
+++ src/app/_utilities/contentFetchers.ts
@@ -0,0 +1,37 @@
+import { getPayloadHMR } from "@payloadcms/next/utilities";
+import configPromise from "@payload-config";
+
+const payload = await getPayloadHMR({ config: configPromise });
+
+export async function fetchContentBySlug({ slug, type }) {
+
+  if (!slug || !type) {
+    throw new Error("Must input slug and/or type.");
+  }
+
+  try {
+    return await payload.find({
+      collection: type,
+      limit: 1,
+      overrideAccess: false,
+      draft: false,
+      where: { slug: { equals: slug } },
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export async function fetchAllContentByType(type) {
+  try {
+    return await payload.find({
+      collection: type,
+      limit: 100,
+      overrideAccess: false,
+      draft: false,
+    })
+      .then(res => res.docs);
+  } catch (err) {
+    throw new Error(err);
+  }
+}
