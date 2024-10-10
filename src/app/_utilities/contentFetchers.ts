1f84bdc9ac38cac284914f10c8e66587b719a46d
Rui Sousa <contact@ruisousa.me>
2024-10-10 18:15:41 +0100

    refactor fetchers

--- src/app/_utilities/contentFetchers.ts
+++ src/app/_utilities/contentFetchers.ts
@@ -1,67 +1,92 @@
 import { getPayloadHMR } from "@payloadcms/next/utilities";
 import configPromise from "@payload-config";
-import type { Config } from "@/payload-types";
+import type { Author, Config, Media } from "@/payload-types";
+import { notFound } from "next/navigation";
+import { getPayload } from "payload";
 
-const payload = await getPayloadHMR({ config: configPromise });
+interface FetcherArgs {
+  collection?: keyof Config["collections"],
+  limit?: number,
+  depth?: number,
+  draft?: boolean,
+  overrideAccess?: boolean,
+  query?: any
+}
 
+async function fetcher({ collection, limit = 10, depth = 1, draft = false, overrideAccess = false, query }: FetcherArgs) {
+  const payload = await getPayloadHMR({ config: configPromise });
+  return await payload.find({
+    collection,
+    limit,
+    depth,
+    draft,
+    overrideAccess,
+    where: query,
+  });
+}
 
-export async function fetchContentBySlug({ slug, type }) {
+export async function fetchContentBySlug({ slug, type }: { slug: string, type: keyof Config["collections"] }) {
+
   if (!slug || !type) {
     throw new Error("Must input slug and/or type.");
   }
 
-  try {
-    return await payload.find({
-      collection: type,
-      limit: 1,
-      overrideAccess: false,
-      draft: false,
-      where: { slug: { equals: slug } },
-    }).then(res => res.docs[0]);
-  } catch (err) {
-    throw new Error(err);
-  }
-};
+  const query = { slug: { equals: slug } };
 
-
-export async function fetchContentFromAuthor(author) {
-  return await payload.find({
-    collection: 'podcasts',
-    limit: 10,
-    where: { authors: { in: author.id  } }
-  }).then(res => res.docs).then(res => {return {Blogposts: res}});
+  return await fetcher({
+    collection: type,
+    query: query,
+  }).then(res => {
+    if (res.docs.length < 1) {
+      notFound();
+    }
+    return res.docs[0];
+  });
 }
 
-export async function fetchAllContentByType(type) {
 
-  try {
-    return await payload.find({
-      collection: type,
-      limit: 100,
-      overrideAccess: false,
-      draft: false,
-    })
-      .then(res => res.docs);
-  } catch (err) {
-    throw new Error(err);
-  }
+export async function fetchContentFromAuthor(author: Author) {
+
+  const query = { authors: { in: author.id } };
+
+  const blogposts = await fetcher({
+    collection: "blogposts",
+    query: query,
+  }).then(res => res.docs);
+
+  const podcasts = await fetcher({
+    collection: "podcasts",
+    query: query,
+  }).then(res => res.docs);
+
+  const cases = await fetcher({
+    collection: "case-studies",
+    query: query,
+  }).then(res => res.docs);
+
+  const talks = await fetcher({
+    collection: "talks-and-roundtables",
+    query: query,
+  }).then(res => res.docs);
+
+  return {
+    Blogposts: blogposts,
+    Podcasts: podcasts,
+    CaseStudies: cases,
+    TalksAndRoundTables: talks,
+  };
 }
 
-// export async function fetchStuff(
-//   { collection, limit = 100, overrideAccess = false, draft = false, variables }: { collection: string, limit: number, overrideAccess: boolean, draft: boolean, variables: any } {
-//     id: string,
-//       name: string,
-//       title: string,
-//     author:
-//
-// }
-// )
-
-
-
+export async function fetchAllContentByType(type: keyof Config["collections"]) {
+  return await fetcher({
+    collection: type,
+    limit: 100,
+    depth: 3,
+  }).then(res => res.docs);
+}
 
 export async function fetchGlobals(slug: keyof Config["globals"], depth: number = 0) {
-
+  const payload = await getPayloadHMR({ config: configPromise });
   return await payload.findGlobal({
     slug: slug,
     depth: depth,
@@ -69,14 +94,14 @@ export async function fetchGlobals(slug: keyof Config["globals"], depth: number 
 }
 
 export async function fetchMediaByID(id) {
-
-  return await payload.find({
+  const query = { where: { id: { equals: id } } };
+  return await fetcher({
     collection: "media",
-    limit: 1,
-    where: { id: { equals: id } },
-  }).then(res => res.docs[0])
+    query: query,
+  }).then((res: { docs: Media[] }) => {
+    return res.docs[0];
+  })
     .then(res => {
       return { filename: res.filename, mimeType: res.mimeType };
-    })
-
+    });
 }
