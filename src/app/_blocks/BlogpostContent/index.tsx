2d3ce459aa2682a388dd72125fc5bc9691fea4a3
Rui Sousa <contact@ruisousa.me>
2024-10-11 15:08:49 +0100

    fix blogpost featured image positioning

--- src/app/_blocks/BlogpostContent/index.tsx
+++ src/app/_blocks/BlogpostContent/index.tsx
@@ -1,17 +1,19 @@
-'use client'
+"use client";
 
-import { Blogpost } from '@/payload-types'
-import FeaturedImage from '../../_components/FeaturedImage'
-import styles from './styles.module.css'
+import { Blogpost } from "@/payload-types";
+import FeaturedImage from "../../_components/FeaturedImage";
+import styles from "./styles.module.css";
 import RichText from "@/components/RichText";
 
 export default function BlogpostContent({ blogpost }) {
-  const { summary, featuredImage } = blogpost
+  const { summary, featuredImage } = blogpost;
   return (
     <div className={styles.container}>
-      {featuredImage && <FeaturedImage className={styles.featuredImage} src={featuredImage} />}
+      <div className={styles.featuredImage}>
+        {featuredImage && <FeaturedImage src={featuredImage} />}
+      </div>
       <div className={styles.summary}>{summary}</div>
-      <RichText content={blogpost.content}/>
+      <RichText content={blogpost.content} />
     </div>
-  )
+  );
 }
