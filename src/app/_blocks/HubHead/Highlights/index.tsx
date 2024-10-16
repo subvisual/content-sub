80b28871845ad2de493155e6d64439e4aa20f764
Rui Sousa <contact@ruisousa.me>
2024-10-16 17:39:36 +0100

    make highlighted content clicable and link to content

--- src/app/_blocks/HubHead/Highlights/index.tsx
+++ src/app/_blocks/HubHead/Highlights/index.tsx
@@ -1,10 +1,10 @@
-
 import CategoryPill from "../../../_components/CategoryPill";
 import { SpectaclesIcon } from "../../../_icons/icons";
 import { estimateReadTime } from "../../../_utilities/estimateReadTime";
 import { formatDateTime } from "../../../_utilities/formatDateTime";
 import styles from "./styles.module.css";
 import Authors from "@/app/_components/Authors";
+import Link from "next/link";
 
 const placeholder = {
   title: "Placeholder",
@@ -27,23 +27,25 @@ export async function Highlights({ content, main }) {
         className={`${styles.highlights} ${main ? styles.mainHighlight : styles.secondaryHighlight}`}
       >
         <p className={styles.overline}> HIGHLIGHTS </p>
-        <h6> {title} </h6>
+        {/* TODO: Adapt to other types of content as well */}
+        <Link href={`./blogposts/${content.slug}`}>
+          <h6> {title} </h6>
+        </Link>
         {content === placeholder ? (<h5>Please setup the highlights</h5>) : (
           <>
             <div className={styles.metadataContainer}>
-              {/* TODO: is this a good approach for multi category items? */}
-              {categories.length > 2 ? (
-                <CategoryPill title={categories[0].title} />
-              ) : (
-                categories.map((category, i) => <CategoryPill key={i} title={category.title} />)
-              )}
 
-              <span className={styles.publishedDate}>{formatDateTime(publishedAt)}</span>
+              <span className={styles.categories}>
+              {categories.map((category, i) => <CategoryPill key={i} title={category.title} />)}
+              </span>
+
+
               <span className={styles.readTime}>
-          <SpectaclesIcon
-            width={"32"}
-            color={main ? "var(--soft-white-100)" : "var(--dark-rock-800)"}
-          />
+                {formatDateTime(publishedAt)}
+                <SpectaclesIcon
+                  width={"32"}
+                  color={main ? "var(--soft-white-100)" : "var(--dark-rock-800)"}
+                />
                 {estimateReadTime("Placeholder")}
         </span>
             </div>
