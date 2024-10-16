633739f3c3f4edebdc9b0e9ae4e6682aa42a7f28
Rui Sousa <contact@ruisousa.me>
2024-10-16 10:31:45 +0100

    update next config
    - add 'localhost:3000' in case CDN_DOMAIN is not defined

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
-const CDN_DOMAIN = process.env.CDN_DOMAIN
+const CDN_DOMAIN = process.env.CDN_DOMAIN || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
