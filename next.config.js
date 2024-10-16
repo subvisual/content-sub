a770ed129c4fab0851d2c20b0c809114cc2078ff
Rui Sousa <contact@ruisousa.me>
2024-10-16 10:05:48 +0100

    update next.config.js to point images to CDN_DOMAIN

--- next.config.js
+++ next.config.js
@@ -3,13 +3,15 @@ import { withPayload } from '@payloadcms/next/withPayload'
 import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const CDN_DOMAIN = process.env.CDN_DOMAIN
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[CDN_DOMAIN /* 'https://example.com' */].map((item) => {
         const url = new URL(item)
+        // console.log('URL IS:',url)
 
         return {
           hostname: url.hostname,
