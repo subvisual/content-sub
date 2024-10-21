258915f2973356cc3e99c8b9f05f37f656ddde44
Rui Sousa <contact@ruisousa.me>
2024-10-21 12:05:33 +0100

    add styling

--- src/app/(pages)/talks-and-roundtables/[slug]/page.tsx
+++ src/app/(pages)/talks-and-roundtables/[slug]/page.tsx
@@ -6,8 +6,9 @@ import { formatDateTime } from "@/app/_utilities/formatDateTime";
 import Share from "@/app/_components/Share";
 import Categories from "@/app/_components/Categories";
 import AuthorPill from "@/app/_components/AuthorPill";
-import Authors from "@/app/_components/Authors";
+import styles from "./styles.module.css";
 import SocialLinks from "@/app/_components/SocialLinks";
+import { Subscribe } from "@/app/_blocks/Subscribe";
 
 export const dynamic = "force-dynamic";
 
@@ -27,52 +28,59 @@ export default async function TalksAndRoundTablesPage({ params: paramsPromise })
     <div>
       {/*{<pre>{JSON.stringify(talk, null, 2)}</pre>}*/}
       {/* Head Block */}
-      <div>
-        <BackButton />
-        <ArchiveButton collection={"talks-and-roundtables"} />
+      <div className={styles.headContainer}>
+        <BackButton color={"var(--soft-white-100)"} />
+        <ArchiveButton collection={"talks-and-roundtables"} color={"var(--soft-white-100)"} />
+        <h5>{talk.title}</h5>
         <p>
           {formatDateTime(talk.publishedAt)}
-          Duration in mins
+          <span>Duration in mins</span>
         </p>
 
       </div>
+      <div className={styles.contentContainer}>
 
-      {/* Contributors column*/}
-      <div>
-        {talk.authors.map((author) => (
-          <>
-            <AuthorPill author={author} />
-            {/* TODO: Enable once most recent PR hits main */}
-            {/*<SocialLinks socials={author.socials} />*/}
-          </>
-        ))}
+        {/* Contributors column*/}
+        <div className={styles.contributors}>
+          <p className={'overline'}>
+            CONTRIBUTORS
+          </p>
+          {talk.authors.map((author) => (
+            <>
+              <AuthorPill author={author} />
+              {/* TODO: Enable once most recent PR hits main */}
+              {/*<SocialLinks socials={author.socials} />*/}
+            </>
+          ))}
+        </div>
 
+        {/* Content column */}
+        <div className={styles.videoPlayer}>
+          {videoID.service === "youtube" && (
+            <iframe
+              width="560"
+              height="315"
+              src={`https://www.youtube.com/embed/${videoID.id}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          )}
+          <h5>About</h5>
+          <p>{talk.summary}</p>
+        </div>
 
+        {/*  Share & categories column */}
+        <div className={styles.sharingAndCategories}>
+          <Share />
+          {talk.categories.length > 0 && (
+            <Categories categories={talk.categories} />
+          )}
+        </div>
       </div>
-
-      {/* Content column */}
-      <div>
-        {videoID.service === "youtube" && (
-          <iframe
-            width="560"
-            height="315"
-            src={`https://www.youtube.com/embed/${videoID.id}`}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe>
-        )}
-      </div>
-
-      {/*  Share & categories column */}
-      <div>
-        <Share />
-        {talk.categories.length > 0 && (
-          <Categories categories={talk.categories} />
-        )}
-      </div>
+      <Subscribe />
 
     </div>
   );
