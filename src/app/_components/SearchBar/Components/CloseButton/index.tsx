63b46c3f442f32694ac267dacbf7225e93865651
Rui Sousa <contact@ruisousa.me>
2024-10-24 17:00:12 +0100

    rename to MobileCloseButton for clarity

--- src/app/_components/SearchBar/Components/CloseButton/index.tsx
+++ src/app/_components/SearchBar/Components/CloseButton/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.css";
 
-export default function CloseButton() {
+export default function MobileCloseButton() {
   return (<div className={styles.closeButton}>
       <p>Close</p>
       <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
