65d4f0f577697c8b9520ec7683659820b2c65d15
Rui Sousa <contact@ruisousa.me>
2024-10-25 12:46:37 +0100

    refactor(category pill): enable linking to category page
    Enables category pill to link directly to the category content page.
    
    Updates SearchBar to disable link function in order to retain filtering
    behaviour.

--- src/app/_components/CategoryPill/index.tsx
+++ src/app/_components/CategoryPill/index.tsx
@@ -2,39 +2,65 @@
 import styles from "./styles.module.css";
 import { CloseIcon } from "@/app/_icons/icons";
 import React, { useState } from "react";
+import Link from "next/link";
+import { toKebabCase } from "@/app/_utilities/toKebabCase";
 
 interface CategoryPillProps {
   title: string,
   id?: string,
   selected?: boolean,
+  enableLink?: boolean,
   setActiveFilter?: React.Dispatch<React.SetStateAction<boolean>>
   setActiveCategory?: React.Dispatch<React.SetStateAction<string>>
 }
 
-export default function CategoryPill({ title, id, selected = false, setActiveFilter, setActiveCategory }: CategoryPillProps) {
+export default function CategoryPill({ title, id, selected = false, setActiveFilter, setActiveCategory, enableLink = true }: CategoryPillProps) {
 
   const dynamicStyle = {
     "--dynamic-color": selected ? "var(--sub-blue-300)" : "var(--sub-blue-100)",
   } as React.CSSProperties;
 
   return (
-    <div id={id} className={styles.categoryPill} style={dynamicStyle}>
-      {title}
-      {selected && (
-        <button onClick={(e) => {
-          // Stop propagation due to heavily nestes structure
-          e.stopPropagation();
-          if (setActiveFilter) {
-            setActiveFilter(false)
-          }
+    enableLink ? (
+      <Link href={`categories/${toKebabCase(title)}`}>
 
-          if (setActiveCategory) {
-            setActiveCategory("")
-          }
-        }}>
-          <CloseIcon id={id} width="16px" color="currentColor" />
-        </button>
-      )}
-    </div>
+        <div id={id} className={styles.categoryPill} style={dynamicStyle}>
+          {title}
+          {selected && (
+            <button onClick={(e) => {
+              // Stop propagation due to heavily nestes structure
+              e.stopPropagation();
+              if (setActiveFilter) {
+                setActiveFilter(false);
+              }
+
+              if (setActiveCategory) {
+                setActiveCategory("");
+              }
+            }}>
+              <CloseIcon id={id} width="16px" color="currentColor" />
+            </button>
+          )}
+        </div>
+      </Link>) : (
+      <div id={id} className={styles.categoryPill} style={dynamicStyle}>
+        {title}
+        {selected && (
+          <button onClick={(e) => {
+            // Stop propagation due to heavily nestes structure
+            e.stopPropagation();
+            if (setActiveFilter) {
+              setActiveFilter(false);
+            }
+
+            if (setActiveCategory) {
+              setActiveCategory("");
+            }
+          }}>
+            <CloseIcon id={id} width="16px" color="currentColor" />
+          </button>
+        )}
+      </div>
+    )
   );
 }
