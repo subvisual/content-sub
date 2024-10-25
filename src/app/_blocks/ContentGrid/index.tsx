d3bf946203925bd61aa32edd2080680ff70c3ffa
Rui Sousa <contact@ruisousa.me>
2024-10-25 11:31:03 +0100

    refactor: rename AuthorContentGrid to ContentGrid
    Renames the AuthorContentGrid component to ContentGrid as it will be
    used in "contents by category" page.

--- src/app/_blocks/ContentGrid/index.tsx
+++ src/app/_blocks/ContentGrid/index.tsx
@@ -1,14 +1,8 @@
-import {
-  Blogpost,
-  CaseStudy,
-  Podcast,
-  TalksAndRoundtable,
-} from "@/payload-types";
 import ContentCard from "../../_components/ContentCard";
 import { calculateTotalArticles } from "../../_utilities/calculateTotalArticles";
 import styles from "./styles.module.css";
 
-export default function AuthorContentGrid({ content }) {
+export default function ContentGrid({ content }) {
 
   return (
     <div className={styles.gridContainer}>
