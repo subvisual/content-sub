3dbe6527282c07dfced5cd1f70d193300aa56745
Rui Sousa <contact@ruisousa.me>
2024-10-25 12:29:15 +0100

    chore(payload-types.ts): regenerate payload types

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -97,6 +97,8 @@ export interface Media {
 export interface Category {
   id: string;
   title: string;
+  slug?: string | null;
+  slugLock?: boolean | null;
   parent?: (string | null) | Category;
   breadcrumbs?:
     | {
